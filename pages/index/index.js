49cebac7001145f0f4d7d279099b8e3f247cc533
郭先生 <1733405449@qq.com>
2020-07-16 17:09:45 +0800

    g-0716

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,10 +7,59 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    getPhoneNumber(e){
-      console.log(e);
+    object1:[
+      {
+      id:0,
+      src:"../../images/kecheng1.png",
+      text:"第一个"
+    },
+    {
+      id:1,
+      src:"../../images/kecheng2.png",
+      text:"第二个"
+    },
+    {
+      id:2,
+      src:"../../images/kecheng3.png",
+      text:"第三个"
+    },
+    {
+      id:3,
+      src:"../../images/kecheng4.png",
+      text:"第四个"
+    },
+    {
+      id:4,
+      src:"../../images/kecheng5.png",
+      text:"第五个"
+    },
+    {
+      id:5,
+      src:"../../images/kecheng1.png",
+      text:"第六个"
+    },
+    {
+      id:6,
+      src:"../../images/kecheng2.png",
+      text:"第七个"
+    },
+    {
+      id:7,
+      src:"../../images/kecheng3.png",
+      text:"第八个"
+    },
+    {
+      id:8,
+      src:"../../images/kecheng4.png",
+      text:"第九个"
+    },
+    {
+      id:9,
+      src:"../../images/kecheng5.png",
+      text:"第十个"
     }
+  ],
+    
   },
   //事件处理函数
   bindViewTap: function() {
