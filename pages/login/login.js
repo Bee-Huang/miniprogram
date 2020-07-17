a6ea6acf954971526f3bcb0a6db825ecb7adfa03
Bee-Huang <810585158@qq.com>
2020-07-17 15:48:29 +0800

    up

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,14 +5,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userinfo:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+      
+  },
 
+  login_to:function(e){
+    var errmsg=e.detail.errMsg;
+    if(errmsg.indexOf("fail")>0){
+      //用户选择了拒绝
+      console.log("用户拒绝权限")
+      wx.navigateBack({})
+    }else{
+      console.log("用户授权权限")
+      console.log(e.detail)
+      let pages = getCurrentPages(); // 当前页的数据，
+      let prevPage = pages[pages.length - 2]; // 上一页的数据
+      prevPage.setData({
+        rawdata:e.detail.rawData
+      })
+      wx.navigateBack({})
+    }
+  },
+
+  cancel:function(){
+    wx.navigateBack({})
   },
 
   /**
