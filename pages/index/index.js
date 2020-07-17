604809a4ba9bf90b121d6a4b55a573909a6ca0c5
Bee-Huang <810585158@qq.com>
2020-07-17 17:00:57 +0800

    0717最终

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,56 +7,93 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
+    swiper_item:[
+      {
+        id:0,
+        src:"../../images/image1.jpg",
+        name:"第一个"
+      },
+      {
+        id:1,
+        src:"../../images/image2.jpg",
+        name:"第二个"
+      },
+      {
+        id:2,
+        src:"../../images/image3.jpg",
+        name:"第三个"
+      },
+      {
+        id:3,
+        src:"../../images/image1.jpg",
+        name:"第四个"
+      },
+      {
+        id:4,
+        src:"../../images/image2.jpg",
+        name:"第五个"
+      }
+    ],  
     object1:[
       {
       id:0,
       src:"../../images/kecheng1.png",
-      text:"第一个"
+      text:"第一个",
+      // url1: '/pages/no1/no1'
     },
     {
       id:1,
       src:"../../images/kecheng2.png",
-      text:"第二个"
+      text:"第二个",
+      // url1: '/pages/no1/no1'
     },
     {
       id:2,
       src:"../../images/kecheng3.png",
-      text:"第三个"
+      text:"第三个",
+      // url1: '/pages/no1/no1'
     },
     {
       id:3,
       src:"../../images/kecheng4.png",
-      text:"第四个"
+      text:"第四个",
+      // url1: '/pages/no1/no1'
     },
     {
       id:4,
       src:"../../images/kecheng5.png",
-      text:"第五个"
+      text:"第五个",
+      // url1: '/pages/no1/no1'
     },
     {
       id:5,
       src:"../../images/kecheng1.png",
-      text:"第六个"
+      text:"第六个",
+      // url1: '/pages/no1/no1'
     },
     {
       id:6,
       src:"../../images/kecheng2.png",
-      text:"第七个"
+      text:"第七个",
+      // url1: '/pages/no1/no1'
     },
     {
       id:7,
       src:"../../images/kecheng3.png",
-      text:"第八个"
+      text:"第八个",
+      // url1: '/pages/no1/no1'
     },
     {
       id:8,
       src:"../../images/kecheng4.png",
-      text:"第九个"
+      text:"第九个",
+      // url1: '/pages/no1/no1'
     },
     {
       id:9,
       src:"../../images/kecheng5.png",
-      text:"第十个"
+      text:"第十个",
+      // url1: '/pages/no1/no1'
     }
   ],
     
