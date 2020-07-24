7fe8fe5b0879362c0d8ad794b5d70443b713bbf4
Bee-Huang <810585158@qq.com>
2020-07-24 17:24:22 +0800

    0724

--- pages/clothes_type/clothes_type.js
+++ pages/clothes_type/clothes_type.js
@@ -12,6 +12,10 @@ Page({
           {id:6,text:'劳保服',icon:''},{id:7,text:'冲锋衣套装',icon:''},
           {id:8,text:'其他',icon:''},{id:9,text:'裤子',icon:''},
           {id:10,text:'时装',icon:''}],
+    type_small:[],
+    list_big:[],
+
+
     type_son:[{type:'圆领短袖',
     data1:[{text:'QX 190',src:'http://cdn.okktee.com/o_1ebfk97kbqfqvqu9121bdh15dqn.jpg?imageView2/2/w/320/h/320/q/90'},{text:'BMY 5810',src:'http://cdn.okktee.com/o_1ec9lr9hg1fdg14i317i1fevi4t33.jpg?imageView2/2/w/320/h/320/q/90'},{text:'HBT 806',src:'http://cdn.okktee.com/o_1dd24vp641n261e271s6t11jh1f7ah.jpg?imageView2/2/w/320/h/320/q/90'}],
     data2:[{text:'BMY YC190-C',src:'http://cdn.okktee.com/o_1e5rftpoq6i88111jtf1v9n12n01r.jpg?imageView2/2/w/320/h/320/q/90'},{text:'BMY WC190-C',src:'http://cdn.okktee.com/o_1e5rgcbv714ab1hfb1ip1o8f17a2t.jpg?imageView2/2/w/320/h/320/q/90'},{text:'HL 6880',src:'http://img.qudache.cn//pictures/2018-09-07/5b91edc18af23.jpg!labrand'}]
@@ -39,11 +43,64 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+      wx.cloud.init()
+      var that=this
       this.setData({
         nav_id:options.index
       })
-      
-      
+      var type_now=this.data.type[options.index].text
+      const db = wx.cloud.database()
+      db.collection('type')
+      .where({
+        big: type_now
+      })
+      .get({
+        success:function(res) {
+          console.log(res.data[0].small);
+            if(res.data.length==1){
+                var tmp=res.data[0].small.split(",")
+                that.setData({
+                  type_small:tmp
+                })
+                console.log(res.data.length);
+                var i=0;
+                for(i=0;i<tmp.length;i++){
+                  console.log(tmp[i]);
+                  const db2 = wx.cloud.database()
+                  db2.collection('list')
+                  .where({
+                    keyword: type_now+","+tmp[i]
+                  })
+                  .limit(6)
+                  .get({
+                    success:function(res){
+                      console.log(res);
+                        if(res.length>0){
+                          var list_tmp={
+                            type:tmp[i],
+                            data1:[{text:'',src:''},{text:'',src:''},{text:'',src:''}],
+                            data2:[{text:'',src:''},{text:'',src:''},{text:'',src:''}]
+                          }
+                          var i=0;
+                          for(i=0;i<res.length;i++){
+                            if(i<3){
+                              //设置data1
+                              list_tmp.data1[i].text=res.data[i].code
+                              list_tmp.data1[i].src=res.data[i].img_src
+                            }else{
+                              //设置data2
+                              list_tmp.data2[i%3].text=res.data[i].code
+                              list_tmp.data2[i%3].src=res.data[i].img_src
+                            }
+                          }
+                          console.log(list_tmp);
+                        }
+                    }
+                  })
+                }
+            }
+        }
+      })
   },
 
   /**
