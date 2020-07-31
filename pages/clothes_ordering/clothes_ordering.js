f06a987e7b91ab5bbc6415523de914785e95313a
郭先生 <1733405449@qq.com>
2020-07-31 16:23:56 +0800

    0731终

--- pages/clothes_ordering/clothes_ordering.js
+++ pages/clothes_ordering/clothes_ordering.js
@@ -5,86 +5,150 @@ Page({
    * 页面的初始数据
    */
   data: {
+    price: 20,
+    all_number: 0,
+    all_money: 0, 
+    is_select_color:-1,
     color:[
       {
         id:0,
         color:'red',
-        text:'红色'
+        text:'红色',
+        is_select:'false'
       },
       {
         id:1,
         color:'black',
-        text:'黑色'
+        text:'黑色',
+        is_select:'false'
       },
       {
         id:2,
         color:'blue',
-        text:'蓝色'
+        text:'蓝色',
+        is_select:'false'
       },
       {
         id:3,
         color:'green',
-        text:'绿色'
+        text:'绿色',
+        is_select:'false'
       },
       {
         id:4,
         color:'purple',
-        text:'紫色'
+        text:'紫色',
+        is_select:'false'
       },
       {
         id:5,
         color:'orange',
-        text:'橙色'
+        text:'橙色',
+        is_select:'false'
       },
       {
         id:6,
         color:'yellow',
-        text:'黄色'
+        text:'黄色',
+        is_select:'false'
       },
       {
-        id:6,
+        id:7,
         color:'#F5F5F5',
-        text:'白色'
+        text:'白色',
+        is_select:'false'
       },
     ],
     data:[
       {
         id:0,
         size_data:'XS',
-        number_data:'0',
+        number_data:0,
       },
       {
-        id:0,
+        id:1,
         size_data:'S',
-        number_data:'9.9',
+        number_data:0,
       },
       {
-        id:0,
+        id:2,
         size_data:'M',
-        number_data:'19.9',
+        number_data:0,
       },
       {
-        id:0,
+        id:3,
         size_data:'L',
-        number_data:'0',
+        number_data:0,
       },
       {
-        id:0,
+        id:4,
         size_data:'XL',
-        number_data:'0',
+        number_data:0,
       },
       {
-        id:0,
+        id:5,
         size_data:'XXL',
-        number_data:'0',
+        number_data:0,
       },
       {
-        id:0,
+        id:6,
         size_data:'XXXL',
-        number_data:'0',
+        number_data:0,
       },
     ]
 
+  },
+
+  color_click:function(e){
+    console.log(e.currentTarget.dataset.id)
+    this.setData({
+      is_select_color:e.currentTarget.dataset.id
+    })
+  },
+
+  reduce_click:function(e){
+    var number=e.currentTarget.dataset.number;
+    var id=e.currentTarget.dataset.id;
+    var array=this.data.data;
+    for(var i=0;i<=array.length;i++){
+      if(i==id){
+        if(array[i].number_data==0){
+          wx.showToast({
+            title: '一件都没有，减个屁啊',
+            icon: 'none',
+            duration: 2000
+          })
+        }
+        else{
+          array[i].number_data-=1
+          this.data.all_number-=1
+          this.data.all_money=this.data.price*this.data.all_number
+        }
+      }
+    }
+    this.setData({
+      data:array,
+      all_number:this.data.all_number,
+      all_money:this.data.all_money
+    })
+  },
+
+  add_click:function(e){
+    var number=e.currentTarget.dataset.number;
+    var id=e.currentTarget.dataset.id;
+    var array=this.data.data;
+    for(var i=0;i<=array.length;i++){
+      if(i==id){
+        array[i].number_data+=1
+        this.data.all_number+=1
+        this.data.all_money=this.data.price*this.data.all_number
+      }
+    }
+    this.setData({
+      data:array,
+      all_number:this.data.all_number,
+      all_money:this.data.all_money
+    })
   },
 
   /**
